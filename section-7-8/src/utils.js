b513fc0087b799c931a3cbb40b970d69392dd0ff
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-28 19:30:36 -0300

    Section 8 - missing returns statements

--- section-7-8/src/utils.js
+++ section-7-8/src/utils.js
@@ -11,13 +11,13 @@ const myGeolocation = (cityName, callback) => {
 
   request({ url, json: true }, (err, { body }) => {
     if (err) {
-      callback(err)
+      return callback(err, {})
     }
 
     const { features } = body
 
     if (features.length === 0) {
-      callback('A problem has happened', {})
+      return callback('A problem has happened', {})
     }
 
     const { center, place_name } = features[0]
@@ -43,13 +43,13 @@ const myForecast = (lat, lng, callback) => {
 
   request({ url, json: true }, (err, { body }) => {
     if (err) {
-      callback(err)
+      return callback(err, {})
     }
 
     const { error, currently } = body
 
     if (error) {
-      callback(error, {})
+      return callback(error, {})
     }
 
     callback(undefined, currently)
