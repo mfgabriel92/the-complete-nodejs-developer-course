f9107b3538f3a1feae6ba988b4f83223b51a56ab
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-29 15:43:58 -0300

    Section 9 - setup to heroku

--- section-7-8-9/src/server.js
+++ section-7-8-9/src/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const { myGeolocation, myForecast } = require('./utils')
+
 const app = express()
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../template/views')
 const partialsDir = path.join(__dirname, '../template/partials')
+const port = process.env.PORT || 3000
 
 hbs.registerPartials(partialsDir)
 
@@ -57,6 +59,6 @@ app.get('*', (req, res) => {
   res.render('404')
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port http://localhost:3000')
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 })
