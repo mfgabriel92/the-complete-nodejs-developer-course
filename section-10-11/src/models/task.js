6630156368ce224b75ac272a7e231fcb4edbfaa1
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-30 18:53:56 -0300

    Section 11 - add task model plus refactorings

---
+++ section-10-11/src/models/task.js
@@ -0,0 +1,17 @@
+const { model } = require('mongoose')
+const validator = require('validator')
+
+const Task = model('Task', {
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  priority: { 
+    type: Number, 
+    default: 3,
+    min: [1, 'The minimum priority level is 1'],
+    max: [3, 'The maximum priority level is 3'] 
+  },
+  createdAt: { type: Date, default: Date.now()},
+  updatedAt: { type: Date, default: Date.now()},
+})
+
+module.exports = Task
--- section-10-11/src/server.js
+++ section-10-11/src/server.js
@@ -2,20 +2,33 @@ require('./db/mongoose')
 
 const express = require('express')
 const User = require('./models/user')
+const Task = require('./models/task')
 const app = express()
 const port = process.env.PORT || 3000
+const HTTP = require('./utils/httpCodes')
 
 app.use(express.json())
 
-app.post('/api/users', (req, res) => {
-  new User(req.body).save()
-  .then(r => res.send(r))
-  .catch(e => {
-    res.send(400, {
-      code: 400,
+app.post('/api/users', ({ body }, res) => {
+  new User(body)
+  .save()
+  .then(r => res.staus(HTTP.CREATED).send(r))
+  .catch(e => res.staus(HTTP.BAD_REQUEST).send({
+      code: HTTP.BAD_REQUEST,
       error: e.message
     })
-  })
+  )
+})
+
+app.post('/api/tasks', ({ body }, res) => {
+  new Task(body)
+  .save()
+  .then(r => res.staus(HTTP.CREATED).send(r))
+  .catch(e => res.staus(HTTP.BAD_REQUEST).send({
+      code: HTTP.BAD_REQUEST,
+      error: e.message
+    })
+  )
 })
 
 app.listen(port, () => console.log(`Server runing on port ${port}`))
