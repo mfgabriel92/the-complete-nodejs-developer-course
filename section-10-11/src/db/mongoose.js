56ae29c04a1363aa182bd0838e43720f6b2b61b1
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-30 11:02:36 -0300

    Section 11 - mongoose model and save

---
+++ section-10-11/src/db/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager', { useNewUrlParser: true, useCreateIndex: true })
+
+const User = mongoose.model('User', {
+  name: { type: String },
+  age: { type: Number }
+})
+
+const me = new User({
+  name: 'Gabriel',
+  age: 'aaa'
+})
+
+me.save().then(model => {
+  console.log('Success')
+}).catch(e => {
+  console.log('Error:', e)
+})
