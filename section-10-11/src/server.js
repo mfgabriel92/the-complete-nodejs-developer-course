8221ef43f92eed23bc1cb03bd79a991f536557f1
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-30 21:40:40 -0300

    Section 11 - endpoints for reading all and one

--- section-10-11/src/server.js
+++ section-10-11/src/server.js
@@ -1,19 +1,19 @@
 require('./db/mongoose')
 
 const express = require('express')
+const HTTP = require('./utils/httpCodes')
 const User = require('./models/user')
 const Task = require('./models/task')
 const app = express()
 const port = process.env.PORT || 3000
-const HTTP = require('./utils/httpCodes')
 
 app.use(express.json())
 
 app.post('/api/users', ({ body }, res) => {
   new User(body)
   .save()
-  .then(r => res.staus(HTTP.CREATED).send(r))
-  .catch(e => res.staus(HTTP.BAD_REQUEST).send({
+  .then(r => res.status(HTTP.CREATED).send(r))
+  .catch(e => res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
@@ -23,8 +23,37 @@ app.post('/api/users', ({ body }, res) => {
 app.post('/api/tasks', ({ body }, res) => {
   new Task(body)
   .save()
-  .then(r => res.staus(HTTP.CREATED).send(r))
-  .catch(e => res.staus(HTTP.BAD_REQUEST).send({
+  .then(r => res.status(HTTP.CREATED).send(r))
+  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+      code: HTTP.BAD_REQUEST,
+      error: e.message
+    })
+  )
+})
+
+app.get('/api/tasks', ({ body }, res) => {
+  Task.find({})
+  .then(r => res.send(r))
+  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+      code: HTTP.BAD_REQUEST,
+      error: e.message
+    })
+  )
+})
+
+app.get('/api/tasks/:id', ({ params }, res) => {
+  Task.findById(params.id)
+  .then(r => {
+    if (!r) {
+      return res.status(HTTP.NOT_FOUND).send({
+        code: HTTP.NOT_FOUND,
+        error: 'Task not found'
+      })
+    }
+
+    res.send(r)
+  })
+  .catch(e => res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
