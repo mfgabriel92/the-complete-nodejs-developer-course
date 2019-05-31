3be8d3ebe034f64913e0482d687dd0f9a4fada31
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-31 10:46:40 -0300

    Section 11 - update endpoint

--- section-10-11/src/server.js
+++ section-10-11/src/server.js
@@ -9,15 +9,34 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/api/tasks', async ({ body }, res) => {
+  try {
+    res.send(await Task.find({}))
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send(e.message)
+  }
+})
+
+app.get('/api/tasks/:id', async ({ params }, res) => {
+  try {
+    const task = await Task.findById(params.id)
+
+    if (!task) {
+      return res.status(HTTP.NOT_FOUND).send('Task not found')
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send(e.message)
+  }
+})
+
 app.post('/api/users', async ({ body }, res) => {
   try {
     const user = await new User(body).save()
     res.status(HTTP.CREATED).send(user)
   } catch (e) {
-    res.status(HTTP.BAD_REQUEST).send({
-      code: HTTP.BAD_REQUEST,
-      error: e.message
-    })
+    res.status(HTTP.BAD_REQUEST).send(e.message)
   }
 })
 
@@ -26,28 +45,30 @@ app.post('/api/tasks', async ({ body }, res) => {
     const task = await new Task(body).save()
     res.status(HTTP.CREATED).send(task)
   } catch (e) {
-    res.status(HTTP.BAD_REQUEST).send({
-      code: HTTP.BAD_REQUEST,
-      error: e.message
-    })
+    res.status(HTTP.BAD_REQUEST).send(e.message)
   }
 })
 
-app.get('/api/tasks', async ({ body }, res) => {
-
+app.patch('/api/users/:id', async ({ params, body }, res) => {
   try {
-    res.send(await Task.find({}))
+    const user = await User.findByIdAndUpdate(params.id, body, { new: true, runValidators: true })
+
+    if (!user) {
+      return res.status(HTTP.NOT_FOUND).send({
+        code: HTTP.NOT_FOUND,
+        error: 'User not found'
+      })
+    }
+
+    res.send(user)
   } catch (e) {
-    res.status(HTTP.BAD_REQUEST).send({
-      code: HTTP.BAD_REQUEST,
-      error: e.message
-    })
+    res.status(HTTP.BAD_REQUEST).send(e.message)
   }
 })
 
-app.get('/api/tasks/:id', async ({ params }, res) => {
+app.patch('/api/tasks/:id', async ({ params, body }, res) => {
   try {
-    const task = await Task.findById(params.id)
+    const task = await Task.findByIdAndUpdate(params.id, body, { new: true, runValidators: true })
 
     if (!task) {
       return res.status(HTTP.NOT_FOUND).send({
@@ -58,10 +79,7 @@ app.get('/api/tasks/:id', async ({ params }, res) => {
 
     res.send(task)
   } catch (e) {
-    res.status(HTTP.BAD_REQUEST).send({
-      code: HTTP.BAD_REQUEST,
-      error: e.message
-    })
+    res.status(HTTP.BAD_REQUEST).send(e.message)
   }
 })
 
