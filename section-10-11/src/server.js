74ce265d0862a997687a2e89109e05e1ff0dbfe5
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-05-30 22:47:11 -0300

    Section 11 - async/await

--- section-10-11/src/server.js
+++ section-10-11/src/server.js
@@ -9,55 +9,60 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/api/users', ({ body }, res) => {
-  new User(body)
-  .save()
-  .then(r => res.status(HTTP.CREATED).send(r))
-  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+app.post('/api/users', async ({ body }, res) => {
+  try {
+    const user = await new User(body).save()
+    res.status(HTTP.CREATED).send(user)
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
-  )
+  }
 })
 
-app.post('/api/tasks', ({ body }, res) => {
-  new Task(body)
-  .save()
-  .then(r => res.status(HTTP.CREATED).send(r))
-  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+app.post('/api/tasks', async ({ body }, res) => {
+  try {
+    const task = await new Task(body).save()
+    res.status(HTTP.CREATED).send(task)
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
-  )
+  }
 })
 
-app.get('/api/tasks', ({ body }, res) => {
-  Task.find({})
-  .then(r => res.send(r))
-  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+app.get('/api/tasks', async ({ body }, res) => {
+
+  try {
+    res.send(await Task.find({}))
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
-  )
+  }
 })
 
-app.get('/api/tasks/:id', ({ params }, res) => {
-  Task.findById(params.id)
-  .then(r => {
-    if (!r) {
+app.get('/api/tasks/:id', async ({ params }, res) => {
+  try {
+    const task = await Task.findById(params.id)
+
+    if (!task) {
       return res.status(HTTP.NOT_FOUND).send({
         code: HTTP.NOT_FOUND,
         error: 'Task not found'
       })
     }
 
-    res.send(r)
-  })
-  .catch(e => res.status(HTTP.BAD_REQUEST).send({
+    res.send(task)
+  } catch (e) {
+    res.status(HTTP.BAD_REQUEST).send({
       code: HTTP.BAD_REQUEST,
       error: e.message
     })
-  )
+  }
 })
 
 app.listen(port, () => console.log(`Server runing on port ${port}`))
