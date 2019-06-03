dc63c9386a758eace9c953b3c9e58c51346a9e59
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-06-03 18:36:36 -0300

    Section 17 - init

---
+++ section-17/src/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const publicDir = path.join(__dirname, '../public')
+
+app.use(express.static(publicDir))
+
+module.exports = app
